9f78f70b19078f8283d2f3e0bbfaf87983d24029
MatthewZhu01 <124189117+MatthewZhu01@users.noreply.github.com>
2023-02-01 13:42:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='virtualhat',
+setup(name='EmotionalSupportAnimal',
       version='1.0'
       description='CompE Stub Library'
       author='Diana Kim, Matthew Zhu'
       author_email='gk2274@nyu.edu, mjz305@nyu.edu'
-      url='https://github.com/diannakmm/virtualhat',
-      py_modules=['virtualhat']
+      url='https://github.com/diannakmm/EmotionalSupportAnimal',
+      py_modules=['EmotionalSupportAnimal']
