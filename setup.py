aabb7cdcf76ecd7c33d3d836c881e9e012d1e3a9
MatthewZhu01 <124189117+MatthewZhu01@users.noreply.github.com>
2023-02-01 13:55:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='EmotionalSupportAnimal',
       version='1.0'
       description='CompE Stub Library'
-      author='Diana Kim, Matthew Zhu'
-      author_email='gk2274@nyu.edu, mjz305@nyu.edu'
+      author='diannakmm', 'MatthewZhu01'
+      author_email='gk2274@nyu.edu', 'mjz305@nyu.edu'
       url='https://github.com/diannakmm/EmotionalSupportAnimal',
       py_modules=['EmotionalSupportAnimal']
