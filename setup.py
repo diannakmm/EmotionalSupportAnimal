18b42e40853e8f3cc40290b59b68f3f03d5c7b1a
MatthewZhu01 <124189117+MatthewZhu01@users.noreply.github.com>
2023-02-01 13:41:46 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='virtualhat',
+      version='1.0'
+      description='CompE Stub Library'
+      author='Diana Kim, Matthew Zhu'
+      author_email='gk2274@nyu.edu, mjz305@nyu.edu'
+      url='https://github.com/diannakmm/virtualhat',
+      py_modules=['virtualhat']
