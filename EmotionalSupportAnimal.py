4a800965a6cef5b71e5dd3875ec55fb1f041aeb6
MatthewZhu01 <124189117+MatthewZhu01@users.noreply.github.com>
2023-02-15 13:36:49 -0500

    Update EmotionalSupportAnimal.py

--- EmotionalSupportAnimal.py
+++ EmotionalSupportAnimal.py
@@ -1,3 +1,15 @@
 def info():
     '''Prints a basic library description'''
     print("Software library for the EmotionalSupportAnimal project.")
+
+def setupHR():
+    pass
+
+def getBPM():
+    pass
+
+def setuptouch():
+    pass
+
+def gettouch():
+    pass
