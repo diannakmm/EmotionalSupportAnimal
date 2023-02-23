33359843b9da57e763f2725d791d711bd98aa0a7
MatthewZhu01 <mjz305@nyu.edu>
2023-02-23 01:22:50 -0500

    Finished code to measure heart rate using wired pulse sensor, not very consistent because pulse sensor has a lot of interference. Pulse sensor has to be on finger perfectly. Close #8 Close #10 Close #14

--- EmotionalSupportAnimal.py
+++ EmotionalSupportAnimal.py
@@ -5,6 +5,9 @@ def info():
     '''Prints a basic library description'''
     print("Software library for the EmotionalSupportAnimal project.")
 
+spi = spidev.SpiDev()
+spi.open(0,0)
+
 # Establish SPI connection with Bus 0, Device 0
 # Make sure HR sensor if connected to SPI 0
 def setupHR():
@@ -14,23 +17,38 @@ def setupHR():
 # Get data from adc
 def get_adc(channel):
 	r = spi.xfer([1, (8 + channel) << 4, 0])
-	adcout = ((r[1] & 3) << 8) + r[2])
+	adcout = ((r[1] & 3) << 8) + r[2]
 	return adcout
 
 # Should calculate BPM (wired HR sensor)
 def getBPM_wired():
-        prev = 0                #set prev value = 0, no beat
-        num_beats = 0           #val to see how many beats in 10s
-        for n in range (100): #loop 100 times, 100 * 0.1s = 10s
-                output = get_adc(0) #gets pulse sensor data
-                if (output != 0 & prev == 0): #only care when 0 -> 62
+        prev = 0                # set prev value = 0, no beat
+        num_beats = 0           # val to see how many beats in 10s
+        peak = 500
+        bottom = 500
+        new_beat = True
+        for n in range (1000): # loop 1000 times, 1000 * 0.01s = 10s
+                output = get_adc(0) # gets pulse sensor data
+                if (output > peak): # find the peak value
+                        peak = output
+                if (output < bottom): # find the bottom value
+                        bottom = output
+
+                # pulse data always hovers at the peak
+                if (output != peak & output <= peak - 10 & new_beat == True): # when moves away from the peak increase beats
                         num_beats += 1
-                prev = output   #store curr pulse val before looping
-                time.sleep(0.1) #change if frequency isnt fast enough
+                        new_beat = False
+                if (output <= peak & output >= peak - 10 & new_beat == False): # when gets back to peak, new beat
+                        new_beat = True
+                prev = output   # store curr pulse val before looping
+                time.sleep(0.01) # change if frequency isnt fast enough
         #now num_beats should be the number of beats detected in 10s
         #(beats per 10s) * 6 = bpm ... i think?
         bpm = 6 * num_beats
-        return(num_bpm)
+        print("peak = ", peak)
+        print("bottom = ", bottom)
+        print("num beats = ", num_beats)
+        return(bpm)
 
 # Should get BPM from BT sensor
 def getBPM_BT():
