ae1160ea3116dc82b6aafede6a41839e926c1814
MatthewZhu01 <mjz305@nyu.edu>
2023-02-25 18:17:10 -0500

    Finished function to read BPM from bluetooth heart rate sensor. Created new function to convert from hex to integer. Was able to see heart rate increase when doing physical activy and decrease when resting. Bluetooth sensor a lot more consistent than wired pulse sensor. Close #23 Close #9

--- EmotionalSupportAnimal.py
+++ EmotionalSupportAnimal.py
@@ -1,12 +1,15 @@
 import spidev
 import time
+import pexpect
 
 def info():
     '''Prints a basic library description'''
     print("Software library for the EmotionalSupportAnimal project.")
 
+# Establish SPI Bus
 spi = spidev.SpiDev()
 spi.open(0,0)
+
 
 # Establish SPI connection with Bus 0, Device 0
 # Make sure HR sensor if connected to SPI 0
@@ -52,13 +55,23 @@ def getBPM_wired():
 
 # Should get BPM from BT sensor
 def getBPM_BT():
-	# delete existing .txt file with all the data if it exists
-	# create new .txt file to store new BT data
-	# read from sensor and push to .txt file
-	# read from file --> second hexadecimal value = HR
+        # Probably will need a seperate function to find BT device ip address
+        Device = "E5:74:B0:F1:CE:9B"
+        # Establish connection to bluetooth device
+        child = pexpect.spawn("sudo gatttool -t random -b {0} -I".format(Device))
+        child.sendline("Connect")
+        child.expect("Connection successful", timeout = 5)
+        # If got here, then connection to BT device was successful
+        # Can just directly read from sensor
+        child.sendline("char-write-req 0x0011 0100")
+        child.expect("Notification handle = 0x0010 value: ", timeout = 10)
+        child.expect("\r\n", timeout = 10)
+        return(hexToInt(child.before[3:5]))
 
-	# might not need to push to txt file and just directly read
-	pass
+# Function to convert hexa-decimal values to integer values
+def hexToInt(hex):
+        val = int(hex[0:2], 16)
+        return val
 
 def setuptouch():
     pass
--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 import EmotionalSupportAnimal as esa
+import time
 
-esa.info()
+#esa.info()
+
+#esa.setupHR()
+
+while True:
+	x = esa.getBPM_BT()
+	print(x)
+	time.sleep(1)
